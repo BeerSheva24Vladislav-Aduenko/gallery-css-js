e3542da4a9e56d13f5a082940c9957d5638ad0cc
VladislavAduenko <adyenko.job@gmail.com>
2025-05-07 22:43:33 +0300

    finish hw

--- src/components/GalleryComponent.js
+++ src/components/GalleryComponent.js
@@ -12,8 +12,8 @@ function getThumbnailItems(data) {
   const res = data.map(getItem);
   return res.join("");
 }
-function getItem({ description, name, reference_image_id }) {
-  const res = `<li class="gallery_item" data-detailed-image="${name} data-detailed-title="${description}">
+function getItem({ description, name, reference_image_id }) {    
+  const res = `<li class="gallery_item" data-detailed-image="${`https://cdn2.thecatapi.com/images/${reference_image_id}.jpg`}" data-detailed-title="${description}">
                <img src="${`https://cdn2.thecatapi.com/images/${reference_image_id}.jpg`}" alt="${name}" class="gallery_item_image">
                 <figcaption class="gallery_item_title">${name}</figcaption>
                </li>`;
