90f1d96d99d702c7308f44c454452af2bd49d950
VladislavAduenko <adyenko.job@gmail.com>
2025-05-12 10:26:39 +0300

    different image files for different sizes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,2 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const galleryItems = document.querySelectorAll(".gallery_item");
-  const detailedImage = document.querySelector(".detailedContainer_image");
-  const detailedTitle = document.querySelector(".detailedContainer_title");
-
-  galleryItems.forEach((item) => {
-    item.addEventListener("click", () => {
-      const img = item.querySelector("img");
-      const text = item.querySelector(".gallery_item_description");
-
-      detailedImage.src = img.src;
-      detailedTitle.textContent = text.textContent;
-    });
-  });
-});
+const ar = Array.from({length: 50}, () => Math.floor(Math.random() * 2));
+console.log(ar);
